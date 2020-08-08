92ead190716c4dcc1c629675afc510206ebbbfda
emilynguyen <emilyn@berkeley.edu>
2020-08-07 23:24:34 -0700

    more work

---
+++ crowd simulation/Assets/Generator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public float particleW;
+    public float particleH;
+    public GameObject particle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider box = GetComponent<BoxCollider>();
+        Vector3 size = box.bounds.max - box.bounds.min;
+
+        for (int i = 0; i < size.x / particleW; i++)
+        {
+            for (int j = 0; i < size.y / particleH; j++)
+            {
+                Instantiate(particle, new Vector3(i * particleW, 1, j * particleH), Quaternion.identity, transform.parent);
+                Debug.Log("help!");
+            }
+        }
+        Debug.Log("done!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- crowd simulation/Assets/Pathing.cs
+++ crowd simulation/Assets/Pathing.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Pathing : MonoBehaviour
 {
+    NavMeshAgent nm;
+    public Transform destination;
     // Start is called before the first frame update
     void Start()
     {
-        
+        nm = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!nm.hasPath)
+        {
+            nm.SetDestination(destination.position);
+
+        }
     }
 }
