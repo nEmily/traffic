0541675f04f21ca3d4fc97ab546d3796b979c25d
emilynguyen <emilyn@berkeley.edu>
2020-08-07 22:47:50 -0700

    website

---
+++ crowd simulation/Assets/Particle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    [HideInInspector] public ParticleSystem system;
+    [ReadOnly] public Vector3 position;
+    [ReadOnly] public Vector3 last_position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        system.AddParticle(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
