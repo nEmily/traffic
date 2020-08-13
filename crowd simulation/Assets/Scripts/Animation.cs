b8ac69850eb5825381a4118b48a6fbb12a77a3f6
emilynguyen <emilyn@berkeley.edu>
2020-08-13 01:59:16 -0700

    adding assets

---
+++ crowd simulation/Assets/Scripts/Animation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Animation : MonoBehaviour
+{
+    [SerializeField] private Animator m_animator;
+    NavMeshAgent nm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator.SetBool("Grounded", true);
+        nm = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_animator.SetFloat("MoveSpeed", nm.velocity.magnitude);
+
+    }
+}
---
+++ crowd simulation/Assets/Scripts/Pathing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pathing : MonoBehaviour
+{
+    public NavMeshAgent nm;
+    public Transform dest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nm = GetComponent<NavMeshAgent>();
+        nm.SetDestination(dest.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetDest(Vector3 dest)
+    {
+        nm = GetComponent<NavMeshAgent>();
+        nm.SetDestination(dest);
+    }
+}
